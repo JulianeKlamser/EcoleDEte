16ccf3292a1fbedb0ce91a54ddb291292af9ac0b
JulianeUta <Juliane_klamser@yahoo.de>
2019-07-28 18:27:07 +0200

    first codes

---
+++ Code00a.py
@@ -0,0 +1,13 @@
+import sys
+
+mot = raw_input("\n\nVeuillez entrer votre nom et tapez enter : ")
+print "\n*****\nBienvenue a l'ecole d'ete", mot, " !"
+mot2 = raw_input("\nTepez ENTER pour continuer !")
+
+print "\n*****\nLa langue de programmation est l'anglais. Les caracteres francais ne peuvent pas etre utilises dans les codes, meme pas dans les commentaires !\n\nQu'est-ce qu'un commentaire ?\nOuvrez Code00.py et regardez la ligne 9 et les lignes 11 a 13.\n\nExcersice : Ajoutez votre propre commentaire et executez a nouveau le programme. Convainquez-vous que le rendement du programme reste inchange. Attention, vous devez d'abord sauvegarder le code modifie ! \n\nPassez ensuite a Code00b.py.\n\n"
+
+# Je suis un commentaire et ne suis donc pas execute par le code. Je commence par un hashtag.
+
+''' Je suis un commentaire sur plusieurs lignes.
+Je commence par trois apostrophes et
+je termine par trois apostrophes.'''
